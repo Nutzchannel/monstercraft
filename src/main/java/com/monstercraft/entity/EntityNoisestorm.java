b2380a67c5de6c922d797e1a83fa1d40c6f0dae6
Spencer <spencie01@gmail.com>
2014-09-06 19:20:34 -0400

    Added Trapcraft influence

--- src/main/java/com/monstercraft/entity/EntityNoisestorm.java
+++ src/main/java/com/monstercraft/entity/EntityNoisestorm.java
@@ -1,9 +1,12 @@
 // begin imports
-
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 // end imports
 //begin class
-public class EntityNoisestorm extends EntityMob {
+public class EntityNoisestorm extends RenderLiving {
 	
 }
