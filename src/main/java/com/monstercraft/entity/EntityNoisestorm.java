cd16fc67888dc48722eeae18673d8b08e99a8e1d
Spencer <spencie01@gmail.com>
2014-09-06 19:11:07 -0400

    basis for noisestorm

---
+++ src/main/java/com/monstercraft/entity/EntityNoisestorm.java
@@ -0,0 +1,9 @@
+// begin imports
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+// end imports
+//begin class
+public class EntityNoisestorm extends EntityMob {
+	
+}
