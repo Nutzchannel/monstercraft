1e02ab59aca54325d50f39b79c0e502dc8747f6c
Spencer <spencie01@gmail.com>
2014-09-07 10:45:43 -0400

    Begin - basic item

--- src/main/java/com/nutz/monstercraft/item/RazorSharp.java
+++ src/main/java/com/nutz/monstercraft/item/RazorSharp.java
@@ -1,7 +1,16 @@
+// begin package
+package com.nutz.monstercraft.generic;
+// end package
 // begin imports
-
+import net.minecraft.item.Item;
 // end imports
 // begin code
-
-
+public class RazorSharp extends Item {
+  public static Item item1
+}
+@EventHandler
+public void preInit(FMLPreInitialization preInitEvent) {
+  item1 = new GenericItem();
+  GameRegistry.registerItem(item1, "item1");
+}
 //end code
