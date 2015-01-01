3ebb7d6547075e1f69782013ce46abbcb2ccd411
Spencer <spencie01@gmail.com>
2015-01-01 14:27:32 -0500

    genItem config

--- src/main/java/com/nutz/monstercraft/item/GenericItem.java
+++ src/main/java/com/nutz/monstercraft/item/GenericItem.java
@@ -1,4 +1,13 @@
 package com.nutz.monstercraft.item.GenericItem;
 import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+@EventHandler
+public void preInit(FMLPreInitializationEvent preInitEvent) {
+  item1 = new GenericItem();
+  GameRegistry.registerItem(item1, "Generic nil");
+}
 public class GenericItem extends Item {
+  setMaxStackSize(64); //Don't know why anyone'd want 64 nil items... me, i guess?
+  setCreativeTab(CreativeTabs.tabMisc); //Going to remove after testing setCreativeTab(..);
+  setUnlocalizedName("genericItem");
 }
