76c06f6345c21f45dfb6249c306e46ec30b6cc33
Spencer <spencie01@gmail.com>
2015-03-01 10:11:46 -0500

    Biped model

--- src/main/java/com/nutz/monstercraft/entity/EntityNoisestorm.java
+++ src/main/java/com/nutz/monstercraft/entity/EntityNoisestorm.java
@@ -5,6 +5,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.client.model.ModelBiped
 // end imports
 //begin class
 public class EntityNoisestorm extends RenderLiving {
