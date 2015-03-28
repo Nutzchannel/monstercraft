8190e59f0186d34471813e3279b5187e5bbaf884
Spencer <spencie01@gmail.com>
2015-03-28 13:43:10 -0400

    At least I got Reference.java
    Wow this needs work

---
+++ src/main/java/com/nutz/monstercraft/Reference.java
@@ -0,0 +1,10 @@
+package com.nutz.monstercraft;
+
+public class Reference
+{
+    public static final String MOD_ID = "monstercraft"
+    public static final String MOD_NAME = "Monstercraft mod!"
+    public static final String VERSION = "a0.1"
+};
+// "wow it's hard to code on an iPhone"
+//            - Me 2015
